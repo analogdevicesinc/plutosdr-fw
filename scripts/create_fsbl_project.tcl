e429d24483e67ae7794fbfe1eaf7e6336881eea5
Michael Hennerich <michael.hennerich@analog.com>
2017-10-09 14:04:30 +0200

    scripts/create_fsbl_project.tcl: Avoid warnings and prepare for 2016.4
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- scripts/create_fsbl_project.tcl
+++ scripts/create_fsbl_project.tcl
@@ -1,8 +1,9 @@
 hsi open_hw_design build/system_top.hdf
 set cpu_name [lindex [hsi get_cells -filter {IP_TYPE==PROCESSOR}] 0]
 
-sdk set_workspace ./build/sdk
-sdk create_hw_project -name hw_0 -hwspec build/system_top.hdf
-sdk create_app_project -name fsbl -hwproject hw_0 -proc $cpu_name -os standalone -lang C -app {Zynq FSBL}
-sdk build_project -type all
+sdk setws ./build/sdk
+sdk createhw -name hw_0 -hwspec build/system_top.hdf
+sdk createapp -name fsbl -hwproject hw_0 -proc $cpu_name -os standalone -lang C -app {Zynq FSBL}
+configapp -app fsbl build-config release
+sdk projects -build -type all
 #xsdk -batch -source create_fsbl_project.tcl
