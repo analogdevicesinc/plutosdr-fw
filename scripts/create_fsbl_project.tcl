2ea3aa8feb0065b0c7d54e230709a680d5915783
Michael Hennerich <michael.hennerich@analog.com>
2022-07-11 17:15:06 +0200

    Revert "Makefile: Update to Vivado Version 2021.1"
    This reverts commit 315422dcd9e42d5065eaddaa891b56dcefc347cc.

--- scripts/create_fsbl_project.tcl
+++ scripts/create_fsbl_project.tcl
@@ -1,4 +1,17 @@
-hsi open_hw_design build/system_top.xsa
+hsi open_hw_design build/system_top.hdf
 set cpu_name [lindex [hsi get_cells -filter {IP_TYPE==PROCESSOR}] 0]
-platform create -name hw0 -hw build/system_top.xsa -os standalone -out ./build/sdk -proc $cpu_name
-platform generate
+
+sdk setws ./build/sdk
+sdk createhw -name hw_0 -hwspec build/system_top.hdf
+
+# Workaround for broken write_sysdev in vivado 2018.2
+catch {
+	set copyfiles [glob ./build/ps7_init*]
+	if {[llength $copyfiles]} {
+		file copy {*}$copyfiles ./build/sdk/hw_0/
+	}
+}
+sdk createapp -name fsbl -hwproject hw_0 -proc $cpu_name -os standalone -lang C -app {Zynq FSBL}
+configapp -app fsbl build-config release
+sdk projects -build -type all
+#xsdk -batch -source create_fsbl_project.tcl
