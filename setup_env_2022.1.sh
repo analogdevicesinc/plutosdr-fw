baffe4349e9e47736a7a95f16bcba9785977befa
Benjamin Menkuec <benjamin@menkuec.de>
2022-08-04 13:37:29 +0200

    add script to set up environment for Vivado/Vitis 2022.1

---
+++ setup_env_2022.1.sh
@@ -0,0 +1,4 @@
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export PATH=$PATH:/tools/Xilinx/Vitis/2022.1/gnu/aarch32/lin/gcc-arm-linu
+x-gnueabi/bin
+export VIVADO_SETTINGS=/tools/Xilinx/Vitis/2022.1/settings64.sh
